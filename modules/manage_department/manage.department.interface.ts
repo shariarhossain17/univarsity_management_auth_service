453ed83ba94876f70d96b8b2b9dfde4e35da70de
shariar17e <shariarhossain23@gmail.com>
2023-12-30 11:53:14 +0600

    create managedepartment model

---
+++ modules/manage_department/manage.department.interface.ts
@@ -0,0 +1,9 @@
+import { Model } from 'mongoose';
+
+export type IManagementDepartment = {
+  title: string;
+};
+export type ManagementDepartmentModel = Model<
+  IManagementDepartment,
+  Record<string, unknown>
+>;
---
+++ modules/manage_department/management.department.model.ts
@@ -0,0 +1,29 @@
+import { Schema, model } from 'mongoose';
+import {
+  IManagementDepartment,
+  ManagementDepartmentModel,
+} from './manage.department.interface';
+
+const managementDepartmentSchema = new Schema<
+  IManagementDepartment,
+  ManagementDepartmentModel
+>(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      virtuals: true,
+    },
+  },
+);
+
+export const ManagementDepartment = model<
+  IManagementDepartment,
+  ManagementDepartmentModel
+>('ManagementDepartmentModel', managementDepartmentSchema);
