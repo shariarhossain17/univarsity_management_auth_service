95a215b3125be27ce388f456ccea14e0e08e3ceb
shariar17e <shariarhossain23@gmail.com>
2023-12-30 18:33:02 +0600

    createa management department zod schema

---
+++ modules/manage_department/manage.department.validate.ts
@@ -0,0 +1,11 @@
+import { z } from 'zod';
+
+const managementDepartmentSchema = z.object({
+  body: z.object({
+    title: z.string({
+      required_error: 'title is required',
+    }),
+  }),
+});
+
+export default managementDepartmentSchema;
